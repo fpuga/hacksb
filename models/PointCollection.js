13cba89822a4ab717a4ae0f9fc694ae00f2c23b8
Francisco Puga <fpuga@icarto.es>
2016-02-27 18:32:07 +0100

    'buscar' filter is working for name and description fields

--- models/PointCollection.js
+++ models/PointCollection.js
@@ -5,11 +5,24 @@ Backbone.HACKSB.PointCollection = Backbone.GeoJson.FeatureCollection.extend({
     url: 'https://fpuga.cartodb.com/api/v2/sql?format=GeoJSON&q=SELECT cartodb_id, the_geom, the_geom_webmercator, description, username, event_tags, event_time, event_type, name FROM hacksb',
 
     filterBy: function(where){
+
       a = this.filter(function(element) {
+        if (! _.isEmpty(where.buscar)) {
+            var donde_buscar_keys = ['name', 'description'];
+            var flag = false;
+            for (var i=0; i < donde_buscar_keys.length; i++) { 
+              if (element.get(donde_buscar_keys[i]).toLowerCase().indexOf(where.buscar.toLowerCase()) != -1) {
+                var properties = _.defaults(element.pick(_.keys(where)), {buscar: where.buscar});
+                flag = flag || _.isEqual(properties, where);
+              } else {
+                flag = flag || false;
+              }
+            }
+            return flag;
+      }
+
+
         var properties = element.pick(_.keys(where));
-        if (properties.utente) {
-          properties.utente = properties.utente.nome;
-        }
         return _.isEqual(properties, where);
       });
       return new Backbone.HACKSB.PointCollection(a);
