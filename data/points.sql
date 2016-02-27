3f55462a8316bc81ec424a0043a5c42bb605167a
Francisco Puga <fpuga@icarto.es>
2016-02-27 15:20:16 +0100

    Adds some fixtures

---
+++ data/points.sql
@@ -0,0 +1,58 @@
+delete from hacksb;
+
+insert into hacksb 
+(description, event_tags, event_type, name, username, the_geom, event_time) 
+values
+('Automático', null, 'Automático', 'Test 1', 'fpuga', CDB_LatLng(42.22584798704646,-8.756875991821289), '2016-02-27T10:10:31Z');
+
+insert into hacksb 
+(description, event_tags, event_type, name, username, the_geom, event_time) 
+values
+('Automático', null, 'Automático', 'Test 2', 'fpuga', CDB_LatLng(42.22807246918665,-8.775415420532227), '2016-02-27T11:10:31Z');
+
+insert into hacksb 
+(description, event_tags, event_type, name, username, the_geom, event_time) 
+values
+('Automático', null, 'Automático', 'Test 3', 'fpuga', CDB_LatLng(42.23086884980937,-8.798246383666992), '2016-02-27T12:10:31Z');
+
+insert into hacksb 
+(description, event_tags, event_type, name, username, the_geom, event_time) 
+values
+('Automático', null, 'Automático', 'Test 4', 'fpuga', CDB_LatLng(42.23322025581026,-8.840045928955078), '2016-02-27T13:10:31Z');
+
+
+insert into hacksb 
+(description, event_tags, event_type, name, username, the_geom, event_time) 
+values
+('Automático', null, 'Automático', 'Test 5', 'fpuga', CDB_LatLng(42.23773216793775,-8.875322341918945), '2016-02-27T14:10:31Z');
+
+insert into hacksb 
+(description, event_tags, event_type, name, username, the_geom, event_time) 
+values
+('Automático', null, 'Automático', 'Test 6', 'fpuga', CDB_LatLng(42.22031822043341,-8.89566421508789), '2016-02-27T15:10:31Z');
+
+insert into hacksb 
+(description, event_tags, event_type, name, username, the_geom, event_time) 
+values
+('Automático', null, 'Automático', 'Test 7', 'fpuga', CDB_LatLng(42.216631440296354,-8.861417770385742), '2016-02-27T16:10:31Z');
+
+
+insert into hacksb 
+(description, event_tags, event_type, name, username, the_geom, event_time) 
+values
+('Automático', null, 'Automático', 'Test 8', 'fpuga', CDB_LatLng(42.21243587693379,-8.81901741027832), '2016-02-27T17:10:31Z');
+
+
+insert into hacksb 
+(description, event_tags, event_type, name, username, the_geom, event_time) 
+values
+('Automático', null, 'Automático', 'Test 9', 'fpuga', CDB_LatLng(42.21644073891737,-8.784341812133789), '2016-02-27T18:10:31Z');
+
+
+
+insert into hacksb 
+(description, event_tags, event_type, name, username, the_geom, event_time) 
+values
+('Perdida de conexión con el dispositivo', null, 'Alerta', 'fpuga a pique', 'fpuga', CDB_LatLng(42.24383226857561,-8.777217864990233), '2016-02-25T18:10:31Z');
+
+
