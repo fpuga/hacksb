efe2179242ecf3f043a93e5582605eb8952bef6c
Francisco Puga <fpuga@icarto.es>
2016-02-27 17:47:21 +0100

    Fixes filters
    Uses a new text input widget for filters. Uses a real mode for
    the where queries.
    
    Fixes a bug with the array of tags

--- models/Where.js
+++ models/Where.js
@@ -2,20 +2,20 @@ Backbone.SIXHIARA = Backbone.SIXHIARA || {};
 Backbone.SIXHIARA.Where = Backbone.Model.extend({
 
   defaults: {
-    'loc_provin': '',
-    'loc_distri': '',
-    'loc_posto':  '',
-    'utente':     '',
-    'lic_tipo':     '',
-    'estado':     '',
-    'pagos':      '',
-    'actividade': '',
+    'cartodb_id':     '',
+    'description':   '',
+    'event_owner':   '',
+    'event_tags': [],
+    'event_time': '',
+    'event_type': '', // 'Automáticao', 'Alerta', 'Usuario'
+    'name':  '',
+    'buscar': ''
   },
 
   values: function(){
     // only return those pairs that are not void
     return _.omit(this.toJSON(), function(value, key, object){
-      return value === '';
+      return _.isEmpty(value);
     });
   },
 
